9fa64d0476340054960332c0b06d22cc29f2f380
Cheremuxa <gaukhar.2006@bk.ru>
2024-08-23 21:35:34 +0600

    copy

---
+++ class1.py
@@ -0,0 +1,4 @@
+from task import NewClass
+
+obj = NewClass()
+obj.tptint()
--- task.py
+++ task.py
@@ -1,3 +1,12 @@
 from art import tprint
 tprint("PYTHON")
 
+class Hello:
+    def tptint(self):
+        print("Hello from the original class")
+class NewClass(Hello):
+    def tptint(self):
+        print("Hello from the NewClass")
+from class1 import NewClass
+obj = NewClass()
+obj.tptint()
